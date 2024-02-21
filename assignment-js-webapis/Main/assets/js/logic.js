970cad76b0a61999b90544633c65917aaee3b9a0
Kelechi Ekwulugo <kekwulugo@gmail.com>
2024-02-20 22:40:05 -0700

    Fix buttons to display correct choices

--- assignment-js-webapis/Main/assets/js/logic.js
+++ assignment-js-webapis/Main/assets/js/logic.js
@@ -10,7 +10,7 @@ let screenClear = document.querySelector(".start");
 let timer = document.getElementById("time");
 let questionTitle = document.getElementById("question-title");
 let choiceButtons = document.getElementById("choices");
-let choicesEl = document.querySelectorAll("button");
+let choicesEl = document.querySelectorAll(".choices");
 let finalScore = document.getElementById("end-screen");
 let startBtn =document.getElementById("start");
 let submitBtn = document.getElementById("submit");
@@ -25,19 +25,15 @@ function startQuiz() {
   screenClear.classList.remove("start");
   screenClear.classList.add("hide");
 
-
   // un-hide questions section
   questionsEl.classList.remove("hide");
   questionsEl.classList.add("start");
 
   // start timer
-  setInterval(function(){
-  time --;
-  },1000)
-
+  clockTick();
+  
   // show starting time
   timer.innerHTML = time;
-
 
   // call a function to show the next question
   getQuestion();
@@ -49,78 +45,75 @@ function getQuestion() {
 
   // update title with current question
   questionTitle.innerHTML = question.title; 
-  
 
   // clear out any old question choices
+  
 
   // loop over the choices for each question
   // get the number of questions
   let numberOfQuestions = questions.length; // assign it the value of the length of the questions array
-  for (let i = 0; i < numberOfQuestions; i++) {
+  for (let i = 0; i < numberOfQuestions-1; i++) {
 
     // create a new button for each choice, setting the label and value for the button
     const newBtnOne = document.createElement("button");
-    newBtnOne.setAttribute("label",questions[i].choices[0]);
-    newBtnOne.setAttribute("value",questions[i].choices[0]);
-    newBtnOne.textContent = questions[i].choices[0];
+    newBtnOne.setAttribute("label",questions[currentQuestionIndex].choices[i]);
+    newBtnOne.setAttribute("value",questions[currentQuestionIndex].choices[i]);
+     newBtnOne.setAttribute("class","choices");
+    newBtnOne.textContent = questions[currentQuestionIndex].choices[i];
     
     //display the choice button on the page
     choiceButtons.appendChild(newBtnOne);
-  }
-}
+  };
+};
 
 function questionClick(event) {
+  console.log(event.target.value);
   // identify the targeted button that was clicked on
- 
- 
-
   // if the clicked element is not a choice button, do nothing.
-  
-  
-  // check if user guessed wrong
-  if (answer != questions[currentQuestionIndex].answer) {
-  // if they got the answer wrong, penalize time by subtracting 15 seconds from the timer
-  time - 15
-  // recall the timer is the score they get at the end
+  //if (event.target.tagName === "BUTTON"){
+    //return false;
 
-  // if they run out of time (i.e., time is less than zero) set time to zero so we can end quiz
-  if(time < 0){
-    time = 0;
-  };
+    // check if user guessed wrong
+   if (buttonValue !== questions[currentQuestionIndex].answer) {
+    
+    // if they got the answer wrong, penalize time by subtracting 15 seconds from the timer
+    // recall the timer is the score they get at the end
+     time - 15;
 
-
-  // display new time on page
-  timer.innerHTML = 0;
+    // if they run out of time (i.e., time is less than zero) set time to zero so we can end quiz
+    if(time < 0){
+      time = 0;
+    };
+     // display new time on page
+  timer.innerHTML = time;
 
   // play "wrong" sound effect
-  let incorrectAudio = incorrect.wav;
-  incorrectAudio.play();
-
+  sfxWrong.play();
 
   // display "wrong" feedback on page
   let feedback = document.createElement("p");
   feedback.setAttribute("class","feedback");
+  feedback.innerHTML = "Wrong";
   questionsEl.appendChild(feedback);
-
-
-} else {
+  } else {
   // play "right" sound effect
-  let correctAudio = correct.wav;
-  correctAudio.play();
-
+  sfxRight.play();
 
   // display "right" feedback on page by displaying the text "Correct!" in the feedback element
   let correctFeeback = document.createElement("p");
+  // set the feedback element to have the class of "feedback"
   correctFeeback.setAttribute("class","feedback");
+  correctFeeback.innerHTML = "Right!"
   questionsEl.appendChild(correctFeeback);
-
-}
-}
+};
+};
+  
 // flash right/wrong feedback on page for half a second
-// set the feedback element to have the class of "feedback"
-
 
 // after one second, remove the "feedback" class from the feedback element
+setInterval(function(){
+  let feedbackElement = document.querySelector(".feedback");
+},1000);
 
 // move to next question
 currentQuestionIndex++;
@@ -129,16 +122,23 @@ currentQuestionIndex++;
 // if the time is less than zero and we have reached the end of the questions array,
 // call a function that ends the quiz (quizEnd function)
 // or else get the next question
+if(currentQuestionIndex > 5 ||  time < 0 ){
+    quizEnd();
+  } else{
+    getQuestion();
+  };
 
 
 // define the steps of the QuizEnd function...when the quiz ends...
 function quizEnd() {
   // stop the timer
+  clearInterval();
 
   // show end screen
   finalScore.setAttribute("class","start");
 
   // show final score
+  finalScore.innerHTML = "The final score is " + time;
   
 
   // hide the "questions" section
@@ -148,12 +148,16 @@ function quizEnd() {
 // add the code in this function to update the time, it should be called every second
 function clockTick() {
   // right here - update time
-
+  setInterval(function(){
+    time--;
   // update the element to display the new time value
-
+  timer.innerHTML = time;
   // check if user ran out of time; if so, call the quizEnd() function
-
-}
+  if(time < 0 ){
+    quizEnd();
+  };
+  },1000);
+};
 
 // complete the steps to save the high score
 function saveHighScore() {
@@ -195,6 +199,7 @@ submitBtn.onclick = saveHighScore;
 startBtn.onclick = startQuiz;
 
 // user clicks on an element containing choices
+
 choicesEl.onclick = questionClick;
 
 //initialsEl.onkeyup = checkForEnter;
